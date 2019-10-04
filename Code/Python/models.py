98c59ad275e7d82a575fac73915b105c445541fd
adabi <amjad.dabi@gmail.com>
2019-10-04 10:47:11 -0500

    Moved files around for organization

--- Code/Python/models.py
+++ Code/Python/models.py
@@ -64,7 +64,7 @@ class ExperimentsModel(QtCore.QAbstractTableModel):
                 else:
                     new_id = max(int(x[0]) for x in self.data) + 1
                     new_id = str(new_id)
-                self.data.append([new_id, new_id, "", "", "", "", "", "", ""])
+                self.data.append([new_id, Any, "", "", "", "", "", "", ""])
                 self.beginInsertRows(QtCore.QModelIndex(), self.number_of_rows, self.number_of_rows)
                 self.insertRow(self.number_of_rows)
                 self.endInsertRows()
@@ -167,9 +167,7 @@ class PlatesModel(QtCore.QAbstractTableModel):
                     new_id = "1"
                 else:
                     new_id = str(int(self.data[-1][0]) + 1)
-                file_path = pathlib.PureWindowsPath(Any)
-                self.data.append([new_id, file_path, ""] + [{x: [] for x in self.concentrations}])
-                file_path = pathlib.Path(file_path).as_posix()
+                self.data.append([new_id, Any, ""] + [{x:[] for x in self.concentrations}])
                 self.beginInsertRows(QtCore.QModelIndex(), self.number_of_rows, self.number_of_rows)
                 self.insertRow(self.number_of_rows)
                 self.endInsertRows()
@@ -179,7 +177,7 @@ class PlatesModel(QtCore.QAbstractTableModel):
                 for concentration in self.concentrations:
                     blank_concentrations += f"[{concentration}]:[],"
                 blank_concentrations = blank_concentrations[:-1]
-                add_plate(self.cell_line, self.experiment, new_id, file_path, "", blank_concentrations)
+                add_plate(self.cell_line, self.experiment, new_id, Any, "", blank_concentrations)
 
             else:
 
@@ -209,7 +207,7 @@ class PlatesModel(QtCore.QAbstractTableModel):
                     self.data[row][column] = Any
                     if field == "file":
                         new_data = pathlib.PureWindowsPath(field)
-                        new_data = str(pathlib.Path(new_data).as_posix())
+                        new_data = str(pathlib.Path(new_data))
                     else:
                         new_data = Any
                     self.dataChanged.emit(QModelIndex, QModelIndex)
